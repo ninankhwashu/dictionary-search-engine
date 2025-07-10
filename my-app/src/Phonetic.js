a37a6745d40a95ecc8758d218206e58098975066
ninankhwashu <ninankhwashu@gmail.com>
2025-07-10 02:03:59 +0200

    Create phonetics display

---
+++ my-app/src/Phonetic.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <h3>{props.phonetic}</h3>
+    </div>
+  );
+}
--- my-app/src/Results.js
+++ my-app/src/Results.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
-  console.log(props.results);
-
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
+        <Phonetic phonetic={props.results.phonetic} />
+
+
 
         {props.results.meanings.map(function (meaning, index) {
           return (
